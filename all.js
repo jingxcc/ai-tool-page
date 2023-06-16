32565001c65b61034a6faf8f28798d8f2da40c2e
michellec <michelle.xj.chen@gmail.com>
2023-06-17 01:22:42 +0800

    partner: swiper, RWD

--- all.js
+++ all.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
+  // scroll to top
   $("#scrollToTop").on("click", (e) => {
     $("html,body").animate({ scrollTop: 0 }, 500);
+  });
+
+  // swiper
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    slidesPerView: 1,
+
+    breakpoints: {
+      // 768: {
+      // },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
 });
