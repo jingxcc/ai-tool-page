56c596a045c26212e82e216ab73f8828b897cf5e
michellec <michelle.xj.chen@gmail.com>
2023-06-23 00:27:17 +0800

    SCSS structure, apply JS, font family
    - SCSS structure: add _base, _utils
    - apply JS in HTML: script defer
    - style: font family, body

--- all.js
+++ all.js
@@ -1,52 +1,52 @@
-$(document).ready(function () {
-  // scroll to top
-  $("#scrollToTop").on("click", (e) => {
-    $("html,body").animate({ scrollTop: 0 }, 500);
-  });
-
-  // swiper
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    slidesPerView: 1,
-
-    breakpoints: {
-      // 768: {
-      // },
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 24,
-      },
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
-  // menu
-  $(".nav-btn").click(function (e) {
-    const defaultButton = "menu";
-    const changeButton = "close";
-    e.preventDefault();
-    $(".nav-menu").toggleClass("show");
-    $(this)
-      .find("span")
-      .text(
-        $(this).find("span").text() === defaultButton
-          ? changeButton
-          : defaultButton
-      );
-  });
-
-  // dropdown
-  $(".tool-dropdown").click((e) => {
-    e.preventDefault();
-    $(".tool-dropdown-menu").toggleClass("d-none");
-  });
-
-  $(".tool-dropdown-item").click(function (e) {
-    e.preventDefault();
-    const selectedItem = $(this).text();
-    $(".tool-dropdown-btn-text").text(selectedItem);
-  });
+// $(document).ready(function () {
+// scroll to top
+$("#scrollToTop").on("click", (e) => {
+  $("html,body").animate({ scrollTop: 0 }, 500);
 });
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: 1,
+
+  breakpoints: {
+    // 768: {
+    // },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// menu
+$(".nav-btn").click(function (e) {
+  const defaultButton = "menu";
+  const changeButton = "close";
+  e.preventDefault();
+  $(".nav-menu").toggleClass("show");
+  $(this)
+    .find("span")
+    .text(
+      $(this).find("span").text() === defaultButton
+        ? changeButton
+        : defaultButton
+    );
+});
+
+// dropdown
+$(".tool-dropdown").click((e) => {
+  e.preventDefault();
+  $(".tool-dropdown-menu").toggleClass("d-none");
+});
+
+$(".tool-dropdown-item").click(function (e) {
+  e.preventDefault();
+  const selectedItem = $(this).text();
+  $(".tool-dropdown-btn-text").text(selectedItem);
+});
+// });
